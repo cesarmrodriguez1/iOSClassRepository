7a1123a0b7877cb88e7adea357f54adba33b48cd
Cesar <cesar.m.rodriguez@tec.mx>
2022-09-30 09:20:21 -0500

    After class 30/09/2022

---
+++ 3) DesignUIKit2/DesignUIKit2/BookCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  BookCollectionViewCell.swift
+//  DesignUIKit2
+//
+//  Created by profesor on 30/09/22.
+//
+
+import UIKit
+
+class BookCollectionViewCell: UICollectionViewCell
+{
+    
+    @IBOutlet weak var bookImageView: UIImageView!
+    
+    
+    
+}
--- 3) DesignUIKit2/DesignUIKit2/FourthViewController.swift
+++ 3) DesignUIKit2/DesignUIKit2/FourthViewController.swift
@@ -8,12 +8,34 @@
 
 import UIKit
 
-class FourthViewController: UIViewController {
+class FourthViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    
+    @IBOutlet weak var BookCollection: UICollectionView!
+    
+    
+    let bookNames = ["Amar es para valientes","Bajo un cielo escarlata", "Breve historia del tiempo", "Cerebro de pan","Despues el anochecer", "El murmullo de las abejas", "El nombre de la rosa", "El Palacio de la Luna", "El ruisenor",
+        "Forex", "Hija de la fortuna", "Investor Coach", "La emperatriz de la luna", "La niebla",
+        "La niña alemana", "La teoria del todo", "La viajera del tiempo", "Los cuatro acuerdos",
+        "Nocturno de la democracia mexicana", "Peregrinos", "Sapiens", "Siempre amigos", "Star Wars"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return bookNames.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let identifier = "Item"
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as! BookCollectionViewCell
+        
+        cell.bookImageView.image = UIImage(named: bookNames[indexPath.row])
+        
+        return cell
+    }
 }
